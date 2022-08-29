4db4d65e9580498f2b3aed80089d7b220898a13a
Oleksii <tornado@gmail.com>
2022-08-29 22:53:23 +0300

    create scroll-up button

--- js/script.js
+++ js/script.js
@@ -96,8 +96,42 @@ $(document).ready(function(){
       $('form').trigger('reset');
     });
     return false;
-
   });
+
+  //scroll-up
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 1500) {
+      $('.scroll-up').fadeIn();
+    } else {
+      $('.scroll-up').fadeOut()
+    }
+  });
+
+  //slow scroll-up
+
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+
 });
 
       
